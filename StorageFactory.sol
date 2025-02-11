bb1d82b22b3022c23e9632f76fb15b47245cb99f
Your Name <dorienjoshuabourne@gmail.com>
2025-02-10 22:02:32 -0500

    Factories

--- StorageFactory.sol
+++ StorageFactory.sol
@@ -1,9 +1,23 @@
 // SPDX-License-Identifier: MIT
-
+// Contracts can deploy other contracts. 
 pragma solidity ^0.8.19;
 
 import "./SimpleStorage.sol";
 
 contract StorageFactory {
-     SimpleStorage[] public listOfSimpleStorageContracts
+     SimpleStorage[] public listOfSimpleStorageContracts;
+
+     function createSimpleStorageContract() public {
+          SimpleStorage SimpleStorgeContractVariable = new SimpleStorage();
+          // SimpleStorage simpleStorage = new SimpleStorage();
+          listOfSimpleStorageContracts.push(SimpleStorgeContractVariable);
+     }
+
+     function sfStore(
+          uint256 _simpleStorageIndex,
+          uint256 _simpleStorageNumber
+     ) public {
+          // Address
+     }         
+     
 }
