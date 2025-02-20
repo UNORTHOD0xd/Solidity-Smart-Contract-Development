e3e5407fa1c80b8c566ed35f38cbf5d65fcbe9d3
Your Name <dorienjoshuabourne@gmail.com>
2025-02-19 20:22:49 -0500

    Call functions

--- Lesson 4/FundMe.sol
+++ Lesson 4/FundMe.sol
@@ -13,6 +13,12 @@ uint256 public constant minimumUsd = 5e18;
 Address[] public funders;
 mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
 
+address public owner;
+
+constructor() {
+    owner = msg.sender;
+}
+
 
 contract FundMe {
     function fund() public payable {
@@ -41,5 +47,29 @@ contract FundMe {
 
     function getVersion() pubilc view returns (uint256) {
        return AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306 ).version();
+    }
+
+    function Withdraw() public {
+        // for loop to reset the funders to 
+        for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++){
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        funders = new address[](0);
+        // There are 3 different methods of sending funds in ether 1.transfer 2.send 3.call
+        // payable(msg.sender) = payable address
+        // msg.sender = address
+        //payable(msg.sender).transfer(address(this).balance); // Transfer uses a max gas of 2300 and reverts the transaction if failed and throws an error.
+        // send
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // require(sendSuccess, "Send failed"); // Send also uses a max gas of 2300 but does not rever the transaction if failed, only returns a bool
+        // call
+       (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+       require(callSuccess, "Call failed"); // call is a low level function used to send funds. 
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Must be contract owner!");
+        _;
     }
 }
